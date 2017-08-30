9fc51203638794245aae706d71b41fb3c044855a
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-08-30 17:38:11 +0200

    Use prettierrc and treat prepush as ci task

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        Recraft
-      </div>
-    );
+    return <div className="App">Recraft</div>;
   }
 }
 
