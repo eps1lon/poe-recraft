22243f6b93e2dce7879ec94ce2661a9d581f745e
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-09-01 14:49:33 +0200

    Fixed type bugs

--- src/poe/interfaces/Applicable.js
+++ src/poe/interfaces/Applicable.js
@@ -1,29 +1,29 @@
 // @flow
 import type FlagSet from '../FlagSet';
-import type ModContainer from '../ModContainer/';
+import type Item from '../ModContainer/Item';
 import type Mod from '../Mod/';
 
 export const mods = (
   collection: any[],
-  mod_container: ModContainer,
-  success: FlagSet
+  item: Item,
+  success: string[]
 ): Mod[] => {
   return collection.filter(thing => {
     return (
       typeof thing.applicableTo === 'function' &&
-      thing.applicableTo(mod_container, success)
+      thing.applicableTo(item, success)
     );
   });
 };
 
 export const map = (
   collection: any[],
-  mod_container: ModContainer,
-  success: FlagSet
+  item: Item,
+  success: string[]
 ): Mod[] => {
   for (const thing of collection) {
     if (typeof thing.applicableTo === 'function') {
-      thing.applicableTo(mod_container, success);
+      thing.applicableTo(item, success);
     }
   }
   return collection;
@@ -32,7 +32,7 @@ export const map = (
 export interface Applicable {
   applicable_flags: FlagSet,
 
-  applicableTo(ModContainer, FlagSet): boolean,
+  applicableTo(Item, string[]): boolean,
   resetApplicable(): void,
   applicableCached(): boolean
 }
