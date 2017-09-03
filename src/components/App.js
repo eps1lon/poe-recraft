bf3a54fab577c987f7311a30274dadf6cfce43c7
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-09-03 21:18:34 +0200

    Added init method

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 import './index.css';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.init();
+  }
+
   render() {
     return <div className="App">Recraft</div>;
   }
--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+import { connect, Provider } from 'react-redux';
 
+import {
+  getItems,
+  getBenchoptions,
+  getMetadata,
+  getMods
+} from '../actions/poe';
 import App from '../components/App';
 
-const Root = ({ store }) => {
+const Root = ({ store, init }) => {
   return (
     <Provider store={store}>
-      <App />
+      <App init={init} />
     </Provider>
   );
 };
 
-export default Root;
+const mapDispatchToProps = dispatch => {
+  return {
+    init: () => {
+      dispatch(getItems());
+      dispatch(getBenchoptions());
+      dispatch(getMetadata());
+      dispatch(getMods());
+    }
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Root);
