1dc169af6b388b55dcde9711169ae35e9f4a60c4
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-09-04 10:14:08 +0200

    Removed redbox-react and switched to react-fiber
    redbox wasnt working in the first place and a remnant of c&p from a hmr gist

--- src/index.js
+++ src/index.js
@@ -14,22 +14,6 @@ let render = () => {
 };
 
 if (module.hot) {
-  // Support hot reloading of components
-  // and display an overlay for runtime errors
-  const renderApp = render;
-  const renderError = error => {
-    const RedBox = require('redbox-react');
-    ReactDOM.render(<RedBox error={error} />, rootEl);
-  };
-
-  render = () => {
-    try {
-      renderApp();
-    } catch (error) {
-      renderError(error);
-    }
-  };
-
   module.hot.accept('./containers/Root', () => {
     setTimeout(render);
   });
