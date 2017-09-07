572494a18cb2180d4c65df00d3dc143f0686ddf2
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-09-07 23:36:35 +0200

    Cleaned up code

--- src/reducers/item.js
+++ src/reducers/item.js
@@ -18,70 +18,77 @@ export const initial: State = {
 };
 
 // return types are not type checked by flow!
-const reducer = handleActions(
+export default handleActions(
   {
-    [setItem.toString()](
-      state: State,
-      action: ReduxActionType<typeof setItem>
-    ): State {
-      return {
-        ...state,
-        item: action.payload
-      };
-    },
-    [addMod.toString()](
-      state: State,
-      { payload: mod }: ReduxActionType<typeof addMod>
-    ): State {
-      const { item } = state;
-
-      if (item != null) {
-        console.log(item, '+', mod);
-
-        let crafted = item.addMod(mod);
-        const added = crafted !== item;
-
-        // try at least one time to make more room for mods
-        if (!added) {
-          crafted = crafted.upgradeRarity().addMod(mod);
-        }
-
-        console.log('changed?', crafted !== item);
-
-        return {
-          ...state,
-          item: crafted
-        };
-      } else {
-        return state;
-      }
-    },
-    [removeMod.toString()](
-      state: State,
-      { payload: mod }: ReduxActionType<typeof removeMod>
-    ): State {
-      const { item } = state;
-
-      if (item != null) {
-        return {
-          ...state,
-          item: item.removeMod(mod)
-        };
-      } else {
-        return state;
-      }
-    },
-    [setItemClass.toString()](
-      state: State,
-      { payload: item_class }: ReduxActionType<typeof setItemClass>
-    ): State {
-      return {
-        ...state,
-        item_class
-      };
-    }
+    [setItem.toString()]: setItemHandle,
+    [addMod.toString()]: addModHandle,
+    [removeMod.toString()]: removeModHandle,
+    [setItemClass.toString()]: setItemClassHandle
   },
   initial
 );
 
-export default reducer;
+function setItemHandle(
+  state: State,
+  action: ReduxActionType<typeof setItem>
+): State {
+  return {
+    ...state,
+    item: action.payload
+  };
+}
+
+function addModHandle(
+  state: State,
+  action: ReduxActionType<typeof addMod>
+): State {
+  const { payload: mod } = action;
+  const { item } = state;
+
+  if (item != null) {
+    console.log(item, '+', mod);
+
+    let crafted = item.addMod(mod);
+    const added = crafted !== item;
+
+    // try at least one time to make more room for mods
+    if (!added) {
+      crafted = crafted.upgradeRarity().addMod(mod);
+    }
+
+    console.log('changed?', crafted !== item);
+
+    return {
+      ...state,
+      item: crafted
+    };
+  } else {
+    return state;
+  }
+}
+
+function removeModHandle(
+  state: State,
+  { payload: mod }: ReduxActionType<typeof removeMod>
+): State {
+  const { item } = state;
+
+  if (item != null) {
+    return {
+      ...state,
+      item: item.removeMod(mod)
+    };
+  } else {
+    return state;
+  }
+}
+
+function setItemClassHandle(
+  state: State,
+  { payload: item_class }: ReduxActionType<typeof setItemClass>
+): State {
+  return {
+    ...state,
+    item_class
+  };
+}
