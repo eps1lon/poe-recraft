6a34fa3b5e87ed89b2fab99cbdde0d34c7a05e99
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-09-15 11:26:08 +0200

    Added tests for implicits

--- src/containers/__tests__/Implicits.js
+++ src/containers/__tests__/Implicits.js
@@ -1,8 +1,39 @@
 // @flow
 import { Implicits } from '../';
+import { Mod } from '../../mods/';
+
+import { findByPrimary } from '../../__fixtures__/util';
+
+const mods = require('../../__fixtures__/mods.json');
+
+const sturdy = new Mod(findByPrimary(mods, 1465));
+const plusLevel = new Mod(findByPrimary(mods, 5215));
+const bladesOnHit = new Mod(findByPrimary(mods, 7058));
 
 it('should not crash', () => {
   const implicits = new Implicits([]);
 
   expect(implicits.mods).toHaveLength(0);
+});
+
+it('should know its limits', () => {
+  const implicits = new Implicits([]);
+
+  expect(implicits.maxModsOfType(sturdy)).toBe(-1);
+  expect(implicits.maxModsOfType(plusLevel)).toBe(1);
+  expect(implicits.maxModsOfType(bladesOnHit)).toBe(1);
+});
+
+it('should disallow foreign mods', () => {
+  const implicits = new Implicits([]);
+
+  expect(implicits.addMod(sturdy)).toBe(implicits);
+});
+
+it('should be immutable', () => {
+  const implicits = new Implicits([]);
+  const new_implicits = implicits.addMod(plusLevel);
+
+  expect(new_implicits).not.toBe(implicits);
+  expect(new_implicits.mods).toHaveLength(1);
 });
