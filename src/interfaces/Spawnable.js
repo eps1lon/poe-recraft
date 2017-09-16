3608a45acf9c666f22d81d8310a06ce77bc9b9a6
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-09-16 10:29:38 +0200

    Declare properties as invariant
    don't actually know why but it removes errors

--- src/interfaces/Spawnable.js
+++ src/interfaces/Spawnable.js
@@ -17,8 +17,8 @@ export interface Spawnable {
  */
 export type MaybeSpawnable = {
   // spawnweight(Item)?
-  spawnweightFor?: Item => number,
-  spawnableOn?: Item => Flags<*>,
+  +spawnweightFor?: Item => number,
+  +spawnableOn?: Item => Flags<*>,
 };
 
 /** 
