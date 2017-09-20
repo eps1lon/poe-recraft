8dd0f2d6da7d2b923007adf02c98598354d648e8
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-09-20 09:46:34 +0200

    Implemented Sextants
    currently missing Apprentice, Journeymans difference

---
+++ src/generators/Sextant.js
@@ -0,0 +1,162 @@
+// @flow
+import type { AtlasNode } from '../containers';
+import { Mod } from '../mods';
+import { type ModProps } from '../schema';
+import { type Flags } from '../util/Flags';
+
+import Orb, {
+  type SpawnableFlag as BaseSpawnableFlag,
+  type SpawnableFlags as BaseSpawnableFlags,
+} from './Orb';
+
+export type SpawnableFlag = BaseSpawnableFlag | 'no_adjacents_with_spawnweight';
+export type SpawnableFlags = BaseSpawnableFlags | Flags<SpawnableFlag>;
+
+export class ContextUndefined extends Error {
+  constructor(context: string) {
+    super(`context not set, set ${context}`);
+  }
+}
+
+export class CorruptedState extends Error {
+  constructor(message: string) {
+    super(`corrupted state: ${message}`);
+  }
+}
+
+export default class Sextant extends Orb<Mod, AtlasNode> {
+  atlas: ?(AtlasNode[]);
+
+  static modFilter(mod: ModProps): boolean {
+    return mod.domain === Mod.DOMAIN.ATLAS;
+  }
+
+  static build(mods: ModProps[]): Sextant {
+    return super.build(mods, Sextant.modFilter, Sextant);
+  }
+
+  /**
+   * creates a list of mod that are either
+   * a) already applied to this node because they are directly in range
+   * b) already applied to adjacent nodes and would create duplicates if applied
+   *    to this node
+   * in other words: consider every mod from maps within a sextant distance of 2
+   * @param {AtlasNode} node 
+   * @param {AtlasNode[]} atlas 
+   */
+  static blockedMods(target: AtlasNode, atlas: AtlasNode[]): Mod[] {
+    return target
+      .inSextantRange(atlas, 1)
+      .reduce(
+        (mods, secondary) => mods.concat(secondary.getAllMods(atlas)),
+        [],
+      );
+  }
+
+  applyTo(node: AtlasNode): AtlasNode {
+    const rolled = super.rollMod(node);
+
+    if (rolled !== node) {
+      // something change
+      // reset the context to signal that it has to be mutated
+      this.atlas = undefined;
+    }
+
+    return rolled;
+  }
+
+  // applicable to any node for now
+  // eslint-disable-next-line no-unused-vars
+  applicableTo(node: AtlasNode) {
+    return {};
+  }
+
+  // I'm not sure how it determines the spawnweight
+  // For example a map with no_monster_packs can still roll
+  // a mod with spawnweight 0 for no_monster_packs if adjacent maps can roll it
+  // I don't know which spawnweight it chooses though if
+  // it can choose between multiple weights
+  // for now we just search the adjacents if its 0 and pick first we find
+  findAdjacentWithSpawnweight(mod: Mod, node: AtlasNode): ?AtlasNode {
+    if (this.atlas == null) throw new ContextUndefined('atlas');
+
+    return node.inSextantRange(this.atlas).find(other => {
+      return mod.spawnweightFor(other) > 0;
+    });
+  }
+
+  isModSpawnableOn(mod: Mod, node: AtlasNode): SpawnableFlags {
+    const spawnable_flags = {
+      ...super.isModSpawnableOn(mod, node),
+      no_adjacents_with_spawnweight: false,
+    };
+
+    if (spawnable_flags.spawnweight_zero) {
+      const adjacent_with_spawnweight = this.findAdjacentWithSpawnweight(
+        mod,
+        node,
+      );
+
+      if (adjacent_with_spawnweight === undefined) {
+        spawnable_flags.no_adjacents_with_spawnweight = true;
+      } else {
+        spawnable_flags.spawnweight_zero = false;
+      }
+    }
+
+    return spawnable_flags;
+  }
+
+  modsFor(node: AtlasNode, whitelist: string[] = []) {
+    if (this.atlas == null) throw new ContextUndefined('atlas');
+
+    const blocked = Sextant.blockedMods(node, this.atlas);
+
+    return super
+      .modsFor(node, whitelist)
+      .filter(({ mod }) => {
+        const is_blocked = blocked.includes(mod);
+
+        return !is_blocked;
+      })
+      .map(({ spawnable, spawnweight, mod, ...details }) => {
+        if (spawnable === undefined || spawnweight === undefined) {
+          throw new Error('spawnable is undefined');
+        }
+
+        let spawnweight_with_adjacents: number = spawnweight;
+
+        if (spawnweight === 0 && !spawnable.spawnweight_zero) {
+          // see Sextant.isModSpawnableOn
+          // we took the spawnweight from an adjacent node
+          const adjacent_with_spawnweight = this.findAdjacentWithSpawnweight(
+            mod,
+            node,
+          );
+
+          // this shouldn't be happening spawnweight should be > 0
+          // if the flag is not set. only exception is whitelist
+          const corrupted_state =
+            adjacent_with_spawnweight === undefined &&
+            !whitelist.includes('spawnweight_zero');
+
+          if (corrupted_state) {
+            throw new CorruptedState(
+              'spawnweight should be > 0 if spawnweight_zero is false',
+            );
+          } else {
+            spawnweight_with_adjacents = mod.spawnweightFor(
+              adjacent_with_spawnweight,
+            );
+          }
+        }
+
+        return {
+          mod,
+          spawnable,
+          spawnweight: spawnweight_with_adjacents,
+          ...details,
+        };
+      });
+  }
+}
---
+++ src/generators/__tests__/Sextant.js
@@ -0,0 +1,157 @@
+import { AtlasNode } from '../../containers';
+
+import Sextant from '../Sextant';
+
+const atlas_nodes_props = require('../../__fixtures__/atlas.json');
+const mods = require('../../__fixtures__/mods.json');
+
+let atlas;
+
+const getNode = primary => atlas.find(node => node.props.primary === primary);
+const getNodeIndex = primary =>
+  atlas.findIndex(node => node.props.primary === primary);
+
+const modIds = ({ props: { id } }) => id;
+
+beforeEach(() => {
+  atlas = atlas_nodes_props.map(node_props => new AtlasNode([], node_props));
+});
+
+it('should build', () => {
+  const sextant = Sextant.build(mods);
+
+  expect(sextant).toBeInstanceOf(Sextant);
+  expect(sextant.getAvailableMods()).toMatchSnapshot();
+});
+
+it('should consider maplevel', () => {
+  const sextant = Sextant.build(mods);
+  sextant.atlas = atlas;
+
+  const dunes = getNode(26);
+
+  expect(sextant.modsFor(dunes).every(({ mod }) => mod.props.level <= 72)).toBe(
+    true,
+  );
+
+  const cemetery = getNode(53);
+  const cemetery_mods = sextant.modsFor(cemetery);
+  // all apprentice
+  expect(cemetery_mods.every(({ mod }) => mod.props.level <= 75)).toBe(true);
+  // some journeyman's
+  expect(cemetery_mods.some(({ mod }) => mod.props.level >= 73)).toBe(true);
+
+  const chimera = getNode(120);
+  const chimera_mods = sextant.modsFor(chimera);
+  // all apprentice
+  expect(chimera_mods.every(({ mod }) => mod.props.level <= 83)).toBe(true);
+  // some journeyman's
+  expect(chimera_mods.some(({ mod }) => mod.props.level >= 73)).toBe(true);
+  // some master's
+  expect(chimera_mods.some(({ mod }) => mod.props.level >= 78)).toBe(true);
+});
+
+describe('sextant blocking', () => {
+  it('should work deterministically', () => {
+    const sextant = Sextant.build(mods);
+    sextant.atlas = atlas;
+
+    const getMod = primary =>
+      sextant.mods.find(({ props }) => primary === props.primary);
+
+    // this is the map on which we wanna block
+    const dunes_index = getNodeIndex(26);
+
+    // this is how we achieve it
+    const oasis_index = getNodeIndex(7);
+    const invasion_mod = getMod(8772);
+
+    const arid_index = getNodeIndex(8);
+    const onslaught_mod = getMod(8789);
+
+    const dungeon_index = getNodeIndex(18);
+    const turbo_mod = getMod(8776);
+
+    // pre
+    expect(Sextant.blockedMods(atlas[dunes_index], atlas)).toHaveLength(0);
+
+    atlas[oasis_index] = atlas[oasis_index].addMod(invasion_mod);
+
+    // post
+    expect(atlas[oasis_index].mods).toContain(invasion_mod);
+    expect(Sextant.blockedMods(atlas[dunes_index], atlas)).toContain(
+      invasion_mod,
+    );
+
+    atlas[arid_index] = atlas[arid_index].addMod(onslaught_mod);
+
+    // post
+    expect(atlas[arid_index].mods).toContain(onslaught_mod);
+    expect(Sextant.blockedMods(atlas[dunes_index], atlas)).toContain(
+      invasion_mod,
+    );
+    expect(Sextant.blockedMods(atlas[dunes_index], atlas)).toContain(
+      onslaught_mod,
+    );
+
+    atlas[dungeon_index] = atlas[dungeon_index].addMod(turbo_mod);
+
+    // post
+    expect(atlas[dungeon_index].mods).toContain(turbo_mod);
+    expect(Sextant.blockedMods(atlas[dunes_index], atlas)).toContain(
+      invasion_mod,
+    );
+    expect(Sextant.blockedMods(atlas[dunes_index], atlas)).toContain(
+      onslaught_mod,
+    );
+    expect(Sextant.blockedMods(atlas[dunes_index], atlas)).toContain(turbo_mod);
+
+    const rollable_mods = sextant
+      .modsFor(atlas[dunes_index])
+      .map(({ mod }) => mod);
+    // just to be safe if referentiel equality is broken
+    const rollable_ids = rollable_mods.map(modIds);
+
+    expect(rollable_mods).not.toContain(invasion_mod);
+    expect(rollable_ids).not.toContain(invasion_mod.props.id);
+    expect(rollable_mods).not.toContain(onslaught_mod);
+    expect(rollable_ids).not.toContain(onslaught_mod.props.id);
+    expect(rollable_mods).not.toContain(turbo_mod);
+    expect(rollable_ids).not.toContain(turbo_mod.props.id);
+  });
+});
+
+it('shouldnt mutate its context', () => {
+  const sextant = Sextant.build(mods);
+
+  const dunes = getNode(26);
+
+  expect(() => sextant.applyTo(dunes)).toThrowError(
+    'context not set, set atlas',
+  );
+
+  sextant.atlas = atlas;
+  const rolled = sextant.applyTo(dunes);
+
+  expect(getNode(26)).toBe(dunes);
+  expect(getNode(26)).not.toBe(rolled);
+
+  expect(() => sextant.applyTo(rolled)).toThrowError(
+    'context not set, set atlas',
+  );
+});
+
+it('should consider adjacents for spawnweight if it is zero', () => {
+  const sextant = Sextant.build(mods);
+  sextant.atlas = atlas;
+
+  const getMod = primary =>
+    sextant.mods.find(({ props }) => primary === props.primary);
+
+  const waka_index = getNodeIndex(38);
+  const breach_mod = getMod(8811);
+
+  const rollable_mods = sextant.modsFor(atlas[waka_index]);
+  expect(rollable_mods.map(({ mod }) => mod)).toContain(breach_mod);
+  expect(rollable_mods.every(({ spawnweight }) => spawnweight > 0)).toBe(true);
+});
