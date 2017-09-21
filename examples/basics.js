84fc01cde5331a74bbea7748e9b389e3f74bcbd1
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-09-21 23:43:45 +0200

    Adjusted example with new api

--- examples/basics.js
+++ examples/basics.js
@@ -3,13 +3,9 @@ import { Transmute } from 'poe-mods';
 
 // data has to be provided. see eps1lon/poedb
 const items = require('./items.json');
-const meta_datas = require('./meta_data.json');
 const mods = require('./mods.json');
 
-const greaves = Item.build(
-  items.find(mod => mod.name === 'Iron Greaves'),
-  meta_datas,
-);
+const greaves = Item.build(items.find(mod => mod.name === 'Iron Greaves'));
 
 const transmute = Transmute.build(mods);
 
