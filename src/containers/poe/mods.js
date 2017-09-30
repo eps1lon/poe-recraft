c7829087df33ff7c888c26ee653dd4516e878d7f
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-09-30 09:37:34 +0200

    Fixed import path

--- src/containers/poe/mods.js
+++ src/containers/poe/mods.js
@@ -2,6 +2,6 @@
 import { connect } from 'react-redux';
 
 import { Mods as ModsComponent } from 'components/poe/mods/';
-import modHandles from 'handles/mod';
+import modHandles from '../handles/mod';
 
 export const Mods = connect(null, modHandles)(ModsComponent);
