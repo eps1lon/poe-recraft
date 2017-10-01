bd1ff4ec3c00b30b26e6fd28a8971ebe5af42a6d
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-10-01 17:52:41 +0200

    Added number formatters

---
+++ src/localize/__tests__/formatters.ts
@@ -0,0 +1,45 @@
+import factory from '../formatters';
+
+it('should throw if the specified formatter doesnt exist', () => {
+  expect(() => factory('foobar')).toThrowError("'foobar' not found");
+});
+
+it('should match all the formats', () => {
+  expect(factory('deciseconds_to_seconds')(4)).toBe(40);
+
+  expect(factory('divide_by_one_hundred')(340)).toBeCloseTo(3.4, 5);
+
+  expect(factory('per_minute_to_per_second')(10)).toBeCloseTo(0.1666666, 5);
+
+  expect(factory('milliseconds_to_seconds')(400)).toBeCloseTo(0.4, 5);
+
+  expect(factory('negate')(4)).toBe(-4);
+
+  expect(factory('divide_by_one_hundred_and_negate')(4)).toBeCloseTo(-0.04, 5);
+
+  expect(factory('old_leech_percent')(10)).toBe(2);
+
+  expect(factory('old_leech_permyriad')(200)).toBe(4);
+
+  expect(factory('per_minute_to_per_second_0dp')(120)).toBe('2');
+  expect(factory('per_minute_to_per_second_0dp')(80)).toBe('1');
+  expect(factory('per_minute_to_per_second_0dp')(110)).toBe('2');
+
+  expect(factory('per_minute_to_per_second_2dp')(4)).toBe('0.07');
+  expect(factory('per_minute_to_per_second_2dp')(10)).toBe('0.17');
+  expect(factory('per_minute_to_per_second_2dp')(6)).toBe('0.10');
+
+  expect(factory('per_minute_to_per_second_2dp_if_required')(10)).toBe('0.17');
+  expect(factory('per_minute_to_per_second_2dp_if_required')(60)).toBe('1.0');
+  expect(factory('per_minute_to_per_second_2dp_if_required')(66)).toBe('1.1');
+
+  expect(factory('milliseconds_to_seconds_0dp')(100)).toBe('0');
+
+  expect(factory('milliseconds_to_seconds_2dp')(100)).toBe('0.10');
+  expect(factory('milliseconds_to_seconds_2dp')(110)).toBe('0.11');
+  expect(factory('milliseconds_to_seconds_2dp')(115)).toBe('0.12');
+
+  expect(factory('multiplicative_damage_modifier')(4)).toBe(4);
+
+  expect(factory('60%_of_value')(4)).toBeCloseTo(2.4);
+});
--- src/localize/formatters.ts
+++ src/localize/formatters.ts
@@ -1,1 +1,35 @@
-type Formatter = (value: number, options: {}) => string;
+type Formatter = (value: number) => number | string;
+
+// usually everything in poe is rounded down but in this case
+// it's done properly
+// evidence: life regen rolls 60 - 120 which would result in (1-2)
+// Alyways rounding down would result in virtually no 2 rolls.
+// but there are currenty ~300 amulets with 2 and ~160 with 1 listed on poe.trade
+// reason beeing that the next tier rolls 121-180.
+const formatters: { [key: string]: Formatter } = {
+  deciseconds_to_seconds: n => n * 10,
+  divide_by_one_hundred: n => n / 100,
+  per_minute_to_per_second: n => n / 60,
+  milliseconds_to_seconds: n => n / 1000,
+  negate: n => -n,
+  divide_by_one_hundred_and_negate: n => -n / 100,
+  old_leech_percent: n => n / 5,
+  old_leech_permyriad: n => n / 50,
+  per_minute_to_per_second_0dp: n => (n / 60).toFixed(0),
+  per_minute_to_per_second_2dp: n => (n / 60).toFixed(2),
+  per_minute_to_per_second_2dp_if_required: n => (n / 60).toPrecision(2),
+  milliseconds_to_seconds_0dp: n => (n / 1000).toFixed(0),
+  milliseconds_to_seconds_2dp: n => (n / 1000).toFixed(2),
+  multiplicative_damage_modifier: n => n,
+  '60%_of_value': n => n * 0.6
+};
+
+const identity = n => n;
+
+export default function factory(formatter_id: string): Formatter {
+  const formatter = formatters[formatter_id];
+
+  if (formatter === undefined) throw new Error(`'${formatter_id}' not found`);
+
+  return formatter;
+}
