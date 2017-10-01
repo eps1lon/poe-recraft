8bb20c9ddb21f6394c3d3e91b9403819d4b2984c
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-10-01 15:44:48 +0200

    Added multi-file support

--- scripts/generateLocaleData.js
+++ scripts/generateLocaleData.js
@@ -6,19 +6,33 @@ const path = require('path');
 
 const grammar = require('../src/grammars/generated/stat_descriptions.js');
 
+const readdir = promisify(fs.readdir);
 const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 
 const parser = new Parser(Grammar.fromCompiled(grammar));
 
-readFile(path.join(__dirname, './stat_descriptions_dev.txt'), {
-  encoding: 'utf-8'
-})
-  .then(text => {
-    console.log(typeof text);
-    parser.feed(text);
+const txt_dir = path.join(__dirname, './');
+const json_dir = path.join(__dirname, '../locale-data/json');
 
-    console.log(JSON.stringify(parser.results, null, 2));
-  })
-  .catch(err => {
-    console.log(err);
-  });
+const file = path.join(__dirname, './stat_descriptions_dev.txt');
+
+readdir(txt_dir).then(files => {
+  return Promise.all(
+    files.filter(file => file.endsWith('.txt')).map(file => {
+      return readFile(path.join(txt_dir, file), { encoding: 'utf-8' })
+        .then(text => {
+          parser.feed(text);
+
+          console.log(JSON.stringify(parser.results, null, 2));
+          return writeFile(
+            path.join(json_dir, path.basename(file, '.txt') + '.json'),
+            JSON.stringify(parser.results[0], null, 2)
+          );
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    })
+  );
+});
