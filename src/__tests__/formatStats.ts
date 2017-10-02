f330a41dad986754ec4489f228cc70385e613da1
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-10-02 17:33:37 +0200

    Added more tests
    Seems stable. Still needs some tests in production

--- src/__tests__/formatStats.ts
+++ src/__tests__/formatStats.ts
@@ -24,13 +24,106 @@ const locale: StatLocaleData = {
         formatters: []
       }
     ]
+  },
+  item_generation_cannot_change_suffixes: {
+    stats: ['item_generation_cannot_change_suffixes'],
+    translations: [
+      {
+        matchers: ['#'],
+        text: 'Suffixes Cannot Be Changed',
+        formatters: []
+      }
+    ]
+  },
+  'attack_speed_+%_while_leeching': {
+    stats: ['attack_speed_+%_while_leeching'],
+    translations: [
+      {
+        matchers: [[1, '#']],
+        text: '%1%%% increased Attack Speed while Leeching',
+        formatters: []
+      },
+      {
+        matchers: [['#', -1]],
+        text: '%1%%% reduced Attack Speed while Leeching',
+        formatters: [
+          {
+            id: 'negate',
+            arg: 1
+          }
+        ]
+      }
+    ]
   }
 };
 
 it('should translate single stat line', () => {
+  expect(() =>
+    formatStats([{ id: 'weapon_physical_damage_+%', value: 0 }], locale)
+  ).toThrowError(
+    'matching translation not found for weapon_physical_damage_+%'
+  );
+});
+
+it('should throw if the values dont match', () => {
   expect(
     formatStats([{ id: 'weapon_physical_damage_+%', value: 25 }], locale)
   ).toEqual(['25% increased Physical Damage with Weapons']);
+});
+
+it('should fallback to deep search', () => {
+  const stats = [
+    { id: 'attack_minimum_added_physical_damage', value: 5 },
+    { id: 'attack_maximum_added_physical_damage', value: 13 }
+  ];
+
+  const aliased_locale: StatLocaleData = {
+    aliased: {
+      stats: [
+        'attack_minimum_added_physical_damage',
+        'attack_maximum_added_physical_damage'
+      ],
+      translations: [
+        {
+          matchers: ['#', '#'],
+          text: 'Adds %1% to %2% Physical Damage to Attacks',
+          formatters: []
+        }
+      ]
+    }
+  };
+
+  expect(formatStats(stats, aliased_locale)).toEqual([
+    'Adds 5 to 13 Physical Damage to Attacks'
+  ]);
+});
+
+it('should throw if something was not translated', () => {
+  const stats = [
+    { id: 'weapon_physical_damage_+%', value: 25 },
+    { id: 'attack_minimum_added_physical_damage', value: 5 },
+    { id: 'attack_maximum_added_physical_damage', value: 13 }
+  ];
+
+  const aliased_locale: StatLocaleData = {
+    aliased: {
+      stats: [
+        'attack_minimum_added_physical_damage',
+        'attack_maximum_added_physical_damage'
+      ],
+      translations: [
+        {
+          matchers: ['#', '#'],
+          text: 'Adds %1% to %2% Physical Damage to Attacks',
+          formatters: []
+        }
+      ]
+    }
+  };
+
+  expect(() => formatStats(stats, aliased_locale)).toThrowError(
+    'no descriptions found for weapon_physical_damage_+%'
+  );
 });
 
 it('should translate douple stat lines', () => {
@@ -54,6 +147,20 @@ it('should translate douple stat lines', () => {
   ).toEqual(['Adds 1 to 56 Physical Damage to Attacks']);
 });
 
-it.skip('should translate collections of stats', () => {
-  //
+it('should translate collections of stats', () => {
+  const translated = formatStats(
+    [
+      { id: 'attack_minimum_added_physical_damage', value: 20 },
+      { id: 'attack_maximum_added_physical_damage', value: 300 },
+      { id: 'item_generation_cannot_change_suffixes', value: -3 },
+      { id: 'attack_speed_+%_while_leeching', value: -5 },
+      { id: 'weapon_physical_damage_+%', value: 25 }
+    ],
+    locale
+  );
+
+  expect(translated).toContain('5% reduced Attack Speed while Leeching');
+  expect(translated).toContain('Adds 20 to 300 Physical Damage to Attacks');
+  expect(translated).toContain('Suffixes Cannot Be Changed');
+  expect(translated).toContain('25% increased Physical Damage with Weapons');
 });
