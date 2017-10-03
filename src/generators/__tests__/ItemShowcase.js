a769b6109bfdb27b623ee8bbeffcdf505f2867ca
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-10-03 18:24:49 +0200

    Added testcase for the 1.5.1 change

--- src/generators/__tests__/ItemShowcase.js
+++ src/generators/__tests__/ItemShowcase.js
@@ -19,3 +19,19 @@ it('should not do anything, just showcase', () => {
   expect(showcase.applyTo(greaves)).toBe(greaves);
   expect(showcase.modsFor(greaves).length).toBeGreaterThan(0);
 });
+
+it('should use special rarity', () => {
+  const showcase = new ItemShowcase(mods.all(), craftingbenchoptions.all());
+
+  const life = mods.fromPrimary(198);
+  const greaves = items.fromPrimary(1650).addMod(life);
+
+  // although this item is white and already has a prefix the showcase
+  // considers this item to be rare
+  expect(
+    showcase
+      .modsFor(greaves)
+      .filter(({ mod }) => mod.isPrefix() && !mod.isMasterMod())
+      .map(({ mod }) => mod.props.primary),
+  ).toMatchSnapshot();
+});
