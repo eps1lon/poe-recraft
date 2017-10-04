8d2563807f2d2dd123e2d382dd7dc30bb818ba11
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-10-04 14:15:07 +0200

    Reworked matching algorithm
    Change argument order for better readability
    Added support for interval matching

--- src/translate/__tests__/match.ts
+++ src/translate/__tests__/match.ts
@@ -1,47 +1,75 @@
-import { matchesSingle, matches } from '../match';
+import { Match, matches, matchesSingle } from '../match';
 
 it('matches wildcar values', () => {
-  expect(matchesSingle('#', 5)).toBe(true);
-  expect(matchesSingle('#', -5)).toBe(true);
-  expect(matchesSingle('#', 0)).toBe(true);
+  expect(matchesSingle(5, '#')).toBe(Match.subset);
+  expect(matchesSingle(-5, '#')).toBe(Match.subset);
+  expect(matchesSingle(0, '#')).toBe(Match.subset);
 });
 
 it('matches exact values', () => {
-  expect(matchesSingle(5, 0)).toBe(false);
-  expect(matchesSingle(0, 5)).toBe(false);
-  expect(matchesSingle(5, 3)).toBe(false);
+  expect(matchesSingle(0, 5)).toBe(Match.none);
+  expect(matchesSingle(5, 0)).toBe(Match.none);
+  expect(matchesSingle(3, 5)).toBe(Match.none);
 
-  expect(matchesSingle(5, 5)).toBe(true);
-  expect(matchesSingle(-4, -4)).toBe(true);
-  expect(matchesSingle(0, 0)).toBe(true);
+  expect(matchesSingle(5, 5)).toBe(Match.exact);
+  expect(matchesSingle(-4, -4)).toBe(Match.exact);
+  expect(matchesSingle(0, 0)).toBe(Match.exact);
 });
 
 it('matches ranges', () => {
-  expect(matchesSingle([0, 5], -1)).toBe(false);
-  expect(matchesSingle([0, 5], 8)).toBe(false);
-  expect(matchesSingle([0, 5], 5)).toBe(true);
-  expect(matchesSingle([0, 5], 0)).toBe(true);
+  expect(matchesSingle(-1, [0, 5])).toBe(Match.none);
+  expect(matchesSingle(8, [0, 5])).toBe(Match.none);
+  expect(matchesSingle(5, [0, 5])).toBe(Match.subset);
+  expect(matchesSingle(0, [0, 5])).toBe(Match.subset);
 
-  expect(matchesSingle([5, '#'], 0)).toBe(false);
-  expect(matchesSingle([5, '#'], 7)).toBe(true);
+  expect(matchesSingle(0, [5, '#'])).toBe(Match.none);
+  expect(matchesSingle(7, [5, '#'])).toBe(Match.subset);
 
-  expect(matchesSingle([0, '#'], -1)).toBe(false);
-  expect(matchesSingle([0, '#'], Number.POSITIVE_INFINITY)).toBe(true);
-  expect(matchesSingle([0, '#'], 0)).toBe(true);
-  expect(matchesSingle([0, '#'], 3)).toBe(true);
+  expect(matchesSingle(-1, [0, '#'])).toBe(Match.none);
+  // gets expanded to [inf, inf]
+  expect(matchesSingle(Number.POSITIVE_INFINITY, [0, '#'])).toBe(Match.subset);
+  // whereas # gets expanded to [-inf, inf]
+  expect(matchesSingle('#', [0, '#'])).toBe(Match.superset);
+  expect(matchesSingle(0, [0, '#'])).toBe(Match.subset);
+  expect(matchesSingle(3, [0, '#'])).toBe(Match.subset);
 
-  expect(matchesSingle(['#', 5], -1)).toBe(true);
-  expect(matchesSingle(['#', 5], 6)).toBe(false);
+  expect(matchesSingle(-1, ['#', 5])).toBe(Match.subset);
+  expect(matchesSingle(6, ['#', 5])).toBe(Match.none);
 });
 
-it('returns false if not enough args are proved', () => {
-  expect(matches([[0, 2], [1, 3]], [2])).toBe(false);
-  expect(matches([[0, 2], '#'], [2])).toBe(false);
+it('returns none if not enough args are proved', () => {
+  expect(matches([2], [[0, 2], [1, 3]])).toEqual([Match.subset, Match.none]);
+  expect(matches([2], [[0, 2], '#'])).toEqual([Match.subset, Match.none]);
 });
 
 it('matches with AND', () => {
-  expect(matches([[0, 2], [1, 3]], [2, 0])).toBe(false);
-  expect(matches([[0, 2], [1, 3]], [2, 2])).toBe(true);
+  expect(matches([2, 0], [[0, 2], [1, 3]])).toEqual([Match.subset, Match.none]);
+  expect(matches([2, 2], [[0, 2], [1, 3]])).toEqual([
+    Match.subset,
+    Match.subset
+  ]);
 
-  expect(matches([[0, '#'], [5, '#']], [2, Number.POSITIVE_INFINITY]));
+  expect(matches([2, Number.POSITIVE_INFINITY], [[0, '#'], [5, '#']])).toEqual([
+    Match.subset,
+    Match.subset
+  ]);
+});
+
+it('should match intervals', () => {
+  expect(matchesSingle([1, 3], [1, 3])).toBe(Match.exact);
+
+  expect(matchesSingle([2, 3], [1, 3])).toBe(Match.subset);
+  expect(matchesSingle([2, 3], [1, 4])).toBe(Match.subset);
+
+  expect(matchesSingle([1, 3], [2, 3])).toBe(Match.superset);
+  expect(matchesSingle([1, '#'], [2, 4])).toBe(Match.superset);
+
+  expect(matchesSingle([1, '#'], [0, 3])).toBe(Match.partial_upper);
+  expect(matchesSingle([1, '#'], [0, 1])).toBe(Match.partial_upper);
+
+  expect(matchesSingle([0, 4], [1, 5])).toBe(Match.partial_lower);
+  expect(matchesSingle([4, 5], [5, 7])).toBe(Match.partial_lower);
+
+  expect(matchesSingle([0, 4], [8, 13])).toBe(Match.none);
+  expect(matchesSingle([5, 7], [0, 1])).toBe(Match.none);
 });
--- src/translate/match.ts
+++ src/translate/match.ts
@@ -1,56 +1,64 @@
-import { AnyValue, Matcher, Range, Value } from '../types/StatDescription';
+export type Boundary = number | '#';
+export type BoundedRange = [Boundary, Boundary];
+export type Value = Boundary | BoundedRange;
 
-const isRange = (matcher: Matcher): matcher is Range =>
-  Array.isArray(matcher) && matcher.length === 2;
+type Range = [number, number];
 
-export function matchesSingle(matcher: Matcher, arg: number): boolean {
-  return matches([matcher], [arg]);
+// types of interval overlap
+export enum Match {
+  exact, // A = B
+  subset, // A \subseteq B
+  superset, // A \supseteq B
+  partial_upper, // forall x in A and x in B: forall y in B and y not in A x >= y i.e. A overlaps the upper region of B
+  partial_lower, // forall x in A and x in B: forall y in B and y not in A x <= y i.e. A overlaps the lower region of B
+  none, // A \minus B = A
+  // helper
+  true = exact | subset | superset | partial_lower | partial_upper,
+  false = none
 }
 
-export function matches(matchers: Matcher[], args: number[]) {
-  return matchers.every((matcher, i) => match(matcher, args[i]));
+// does a value match a matcher
+export function matchesSingle(value: Value, matcher: Value): Match {
+  return matches([value], [matcher])[0];
 }
 
-function match(matcher: Matcher, target: number | undefined): boolean {
-  if (target === undefined) {
-    return false;
+export function matches(values: Value[], matchers: Value[]): Match[] {
+  return matchers.map((matcher, i) => match(values[i], matcher));
+}
+
+// interval matching
+function match(value: Value | undefined, matcher: Value): Match {
+  if (value === undefined) {
+    return Match.none;
   }
 
-  if (isRange(matcher)) {
-    return matchRange(matcher, target);
+  const A = rangeCast(value);
+  const B = rangeCast(matcher);
+
+  if (A[0] === B[0] && A[1] === B[1]) {
+    return Match.exact;
+  } else if (A[0] >= B[0] && A[1] <= B[1]) {
+    return Match.subset;
+  } else if (A[0] <= B[0] && A[1] >= B[1]) {
+    return Match.superset;
+  } else if (A[0] >= B[0] && A[0] <= B[1] && A[1] > B[1]) {
+    return Match.partial_upper;
+  } else if (A[1] >= B[0] && A[1] <= B[1] && A[0] < B[0]) {
+    return Match.partial_lower;
   } else {
-    return matchValue(matcher, target);
+    return Match.none;
   }
 }
 
-function matchRange(range: Range, target: number): boolean {
-  return lowerBound(range[0], target) && upperBound(range[1], target);
+function rangeCast(value: Value): Range {
+  const [lower, upper] = isBoundedRange(value) ? value : [value, value];
+
+  return [
+    lower === '#' ? Number.NEGATIVE_INFINITY : lower,
+    upper === '#' ? Number.POSITIVE_INFINITY : upper
+  ];
 }
 
-function matchValue(value: Value, target: number): boolean {
-  if (isAnyValue(value)) {
-    return true;
-  } else {
-    return value === target;
-  }
-}
-
-function lowerBound(value: Value, target: number): boolean {
-  if (isAnyValue(value)) {
-    return true;
-  } else {
-    return value <= target;
-  }
-}
-
-function upperBound(value: Value, target: number): boolean {
-  if (isAnyValue(value)) {
-    return true;
-  } else {
-    return target <= value;
-  }
-}
-
-function isAnyValue(value: Value): value is AnyValue {
-  return value === '#';
+function isBoundedRange(matcher: Value): matcher is BoundedRange {
+  return Array.isArray(matcher) && matcher.length === 2;
 }
