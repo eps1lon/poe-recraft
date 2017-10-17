bad64920b431131cc27f139926f9f8f00ae44488
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-10-17 10:26:57 +0200

    Added dat translations for simplified chinese

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased](https://github.com/eps1lon/poe-mods/compare/v0.4.0...HEAD)
+### Added
+- BaseItemTypes and Mods for Simplified Chinese 
 ### Changed
 - renamed `zh-Hans` to `cn` and `zh-Hant` to `tw` for easier integration with
   other i18n packages.
--- scripts/preGenerateDatLocaleData.sh
+++ scripts/preGenerateDatLocaleData.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
+
 declare -A codes
-codes[de]=German
-codes[en]=English
-codes[es]=Spanish
-codes[fr]=French
-codes[pt]=Portuguese
-codes[ru]=Russian
-codes[th]=Thai
+case "$1" in
+"steam")
+  codes[de]=German
+  codes[en]=English
+  codes[es]=Spanish
+  codes[fr]=French
+  codes[pt]=Portuguese
+  codes[ru]=Russian
+  codes[th]=Thai
+  ;;
+"tencent")
+  codes[cn]="Simplified Chinese"
+  ;;
+*)
+  echo "set distributor as first arg" 1>&2
+  exit 64
+  ;;
+esac
 
 dats='BaseItemTypes Mods'
 
 for code in "${!codes[@]}"
 do
   echo "exporting ${code}"
-  pypoe_exporter dat json tmp/exports/${code}.json --files ${dats} --language ${codes[$code]}
+  pypoe_exporter dat json tmp/exports/${code}.json --files ${dats} --language "${codes[$code]}"
 done
