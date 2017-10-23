7c8ae15452a2aff50dc96b179f73cedef38db5d6
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2017-10-23 13:19:41 +0200

    Added separate dependency graph for descriptions

---
+++ scripts/createDescriptionsDependency.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const fs = require('fs');
+
+const isDescription = file => file.endsWith('stat_descriptions.json');
+
+const locale_dir = path.join(__dirname, '../locale-data/en');
+const out_file = path.join(
+  __dirname,
+  '../src/translate/descriptions_dependency.json'
+);
+
+const dependencies = fs.readdirSync(locale_dir).reduce((dependencies, file) => {
+  if (isDescription(file)) {
+    const { meta: { include } } = require(path.join(locale_dir, file));
+
+    dependencies[path.basename(file, '.json')] = include;
+  }
+  return dependencies;
+}, {});
+
+fs.writeFileSync(out_file, JSON.stringify(dependencies));
