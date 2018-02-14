ec65a1996aef081ef7aa8a49757917830da32388
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-02-14 20:31:16 +0100

    esmodule interop

--- src/item/Item.tsx
+++ src/item/Item.tsx
@@ -1,4 +1,4 @@
-import { PureComponent } from 'react';
+import * as React from 'react';
 
 import { BaseItemProps, ItemProps, Rarity } from './poe';
 import Head from './head';
@@ -9,11 +9,17 @@ export interface Props {
   item: ItemProps;
 }
 
-export default class Item extends PureComponent<Props> {
-  public static assertValidProps(props: Props, onError: (err: string) => void) {
-    const { item } = props;
+export default class Item extends React.PureComponent<Props> {
+  public static assertValidProps(
+    item: ItemProps,
+    onError: (err: string) => void,
+  ) {
     if (item.name === undefined && item.rarity === Rarity.rare) {
       onError('rare items need a name in addition to the name of the baseitem');
+    }
+
+    if (item.elder && item.shaper) {
+      onError('item can either be shaped or elder item but not both');
     }
   }
 
@@ -25,7 +31,7 @@ export default class Item extends PureComponent<Props> {
     super(props);
 
     if (process.env.NODE_ENV !== 'production') {
-      Item.assertValidProps(props, console.warn);
+      Item.assertValidProps(props.item, console.warn);
     }
   }
 
