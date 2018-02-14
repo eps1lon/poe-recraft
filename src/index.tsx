bbd2713f34683f18ad81f8fa62b2d47b8a19e694
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-02-14 21:25:51 +0100

    Added actual package export

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,2 @@
-import React, { PureComponent } from 'react';
-
-export interface Props {
-  bar: string;
-}
-
-export default class Foo extends PureComponent<Props> {
-  render() {
-    return <div>{this.props.bar}</div>;
-  }
-}
+import Item from './item';
+export default Item;
