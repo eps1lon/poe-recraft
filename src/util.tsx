068a439dccb0241cdadb753d07e2c74755823400
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-02-14 21:25:17 +0100

    Fixed reduce usage
    Misunderstood usage. Thought without inital value initial value would be array with 1st but its just the first.

--- src/util.tsx
+++ src/util.tsx
@@ -6,12 +6,19 @@ export const CommaSeparator = () => ', ';
 export const LineBreak = (key: string) => <br key={key} />;
 
 export function intersperse(
-  elements: React.ReactNode[],
+  nodes: React.ReactNode[],
   renderSeparator: (key: string) => React.ReactNode = CommaSeparator,
 ) {
-  return elements.reduce((joined: React.ReactNode[], element, index) => {
-    // push instead of concat's returnval because concat is expensive
-    joined.push(renderSeparator(`separator-${index}`), element);
-    return joined;
-  });
+  if (nodes.length === 0) {
+    return [];
+  }
+
+  return nodes.slice(1).reduce(
+    (joined: React.ReactNode[], element, index) => {
+      // push instead of concat's returnval because concat is expensive
+      joined.push(renderSeparator(`separator-${index}`), element);
+      return joined;
+    },
+    [nodes[0]],
+  );
 }
