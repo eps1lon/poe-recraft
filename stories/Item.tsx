8442a1cedfdd517bea263fb4e09d11dd16ca627f
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-02-14 19:43:17 +0100

    Use weapon from https://www.pathofexile.com/item-data

--- stories/Item.tsx
+++ stories/Item.tsx
@@ -8,13 +8,20 @@ import '../src/themes/poe/style.scss';
 
 const magic_greaves: ItemProps = {
   base: {
-    name: 'Iron Greaves',
+    name: 'Blinder',
   },
   rarity: Rarity.magic,
-  explicits: [{ id: 'Strength1', generation_type: 2, name: 'of Brute' }],
+  explicits: [
+    { id: 'LocalAddedLightningDamage2', generation_type: 1, name: 'Buzzing' },
+    {
+      id: 'LocalIncreasedAttackSpeed3',
+      generation_type: 2,
+      name: 'of Mastery',
+    },
+  ],
 };
 
-storiesOf('magic Iron Greaves', module).add('Head', () => (
+storiesOf('magic item', module).add('Head', () => (
   <div className="poe-item magic">
     <Head item={magic_greaves} />
   </div>
