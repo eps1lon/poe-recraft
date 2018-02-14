6226ceb3adf3bdc4ca2975e037026f021af06c02
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-02-14 20:10:06 +0100

    Added normal, rare and unique stories

--- stories/Item.tsx
+++ stories/Item.tsx
@@ -6,7 +6,14 @@ import Head from './Head';
 
 import '../src/themes/poe/style.scss';
 
-const magic_greaves: ItemProps = {
+const normal_item: ItemProps = {
+  base: {
+    name: 'Full Scale Armour',
+  },
+  rarity: Rarity.normal,
+};
+
+const magic_item: ItemProps = {
   base: {
     name: 'Blinder',
   },
@@ -21,8 +28,61 @@ const magic_greaves: ItemProps = {
   ],
 };
 
-storiesOf('magic item', module).add('Head', () => (
+const rare_item: ItemProps = {
+  base: {
+    name: 'Vaal Regalia',
+  },
+  rarity: Rarity.rare,
+  explicits: [
+    { generation_type: 1, id: 'Sapphire', name: 'Sapphire' },
+    {
+      generation_type: 1,
+      id: 'LocalIncreasedEnergyShieldPercentAndStunRecovery3',
+      name: "Boggart's",
+    },
+    {
+      generation_type: 1,
+      id: 'IncreasedEnergyShieldPercent2',
+      name: 'Strong-Willed',
+    },
+    { generation_type: 2, id: 'FireResist6', name: 'of the Volcano' },
+  ],
+};
+
+const unique_item: ItemProps = {
+  base: {
+    name: 'Vaal Regalia',
+  },
+  rarity: Rarity.rare,
+  explicits: [{ generation_type: 3, id: 'SomeUniqueMod', name: 'Unique' }],
+};
+
+const head_stories = storiesOf('head', module);
+
+// normal item
+head_stories.add('normal', () => (
+  <div className="poe-item normal">
+    <Head item={normal_item} />
+  </div>
+));
+
+// magic item
+head_stories.add('magic', () => (
   <div className="poe-item magic">
-    <Head item={magic_greaves} />
+    <Head item={magic_item} />
+  </div>
+));
+
+// rare item
+head_stories.add('rare', () => (
+  <div className="poe-item rare">
+    <Head item={rare_item} />
+  </div>
+));
+
+// unique item
+head_stories.add('unique', () => (
+  <div className="poe-item unique">
+    <Head item={unique_item} />
   </div>
 ));
