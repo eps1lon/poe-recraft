de05369c93812a5642c0a81111e9cb59bb0df557
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-02-22 18:03:44 +0100

    refactor: moved webpack config into separate folder

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,7 +1,7 @@
 // load the default config generator.
 const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
 const path = require('path');
-const webpack_config = require('../config/webpack.config');
+const webpack_config = require('../config/webpack');
 
 module.exports = (baseConfig, env) => {
   const config = genDefaultConfig(baseConfig, env);
