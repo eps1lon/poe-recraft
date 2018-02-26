1f14f854209fdc5f85a0b75fc35066382b881371
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-02-26 17:20:27 +0100

    Added i18n integration example

--- stories/docs/helmet.tsx
+++ stories/docs/helmet.tsx
@@ -1,43 +1,54 @@
 import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 import { formatStats } from 'poe-i18n';
 // 'poe-components-item'
 import { Popup, Rarity, ModType, ItemType } from '../../src/';
 
-const story = () => (
-  <Popup
-    item={{
-      base: {
-        name: 'Hubris Circlet',
-      },
-      name: 'Mind Brow',
-      type: ItemType.armour,
-      rarity: Rarity.rare,
-      energy_shield: {
-        value: 200,
-        augmented: true,
-      },
-      quality: 6,
-      shaper: true,
-      requirements: {
-        level: {
-          value: 69,
-        },
-        inteliligence: {
-          value: 154,
-        },
-      },
-      explicits: [
-        'Place an additional Mine',
-        'Socketed Gems are Supported by Level 18 Remote Mine',
-        '16% increased Effect of Chill',
-        'Socketed Gems are Supported by Level 18 Hypothermia',
-        '+1 to Level of Socketed Minion Gems',
-        '+67 to maximum Life',
-        '18% increased Stun and Block Recovery',
-        'Adds 5 to 89 Lightning Damage to Spells',
-      ],
-    }}
-  />
-);
+import '../../themes/poe.scss';
 
-export default story;
+storiesOf('ItemPopup', module).add(
+  'Helmet',
+  withInfo({
+    inline: true,
+    source: false,
+    text: `
+    [detailed Item type definition](api/globals.html#item)
+    `,
+  })(() => (
+    <Popup
+      item={{
+        base: {
+          name: 'Hubris Circlet',
+        },
+        name: 'Mind Brow',
+        type: ItemType.armour,
+        rarity: Rarity.rare,
+        energy_shield: {
+          value: 200,
+          augmented: true,
+        },
+        quality: 6,
+        shaper: true,
+        requirements: {
+          level: {
+            value: 69,
+          },
+          inteliligence: {
+            value: 154,
+          },
+        },
+        explicits: [
+          'Place an additional Mine',
+          'Socketed Gems are Supported by Level 18 Remote Mine',
+          '16% increased Effect of Chill',
+          'Socketed Gems are Supported by Level 18 Hypothermia',
+          '+1 to Level of Socketed Minion Gems',
+          '+67 to maximum Life',
+          '18% increased Stun and Block Recovery',
+          'Adds 5 to 89 Lightning Damage to Spells',
+        ],
+      }}
+    />
+  )),
+);
--- stories/docs/index.tsx
+++ stories/docs/index.tsx
@@ -1,18 +1,2 @@
-import * as React from 'react';
-import { storiesOf } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-
-import Helmet from './helmet';
-
-import '../../themes/poe.scss';
-
-storiesOf('ItemPopup', module).add(
-  'Helmet',
-  withInfo({
-    inline: true,
-    source: false,
-    text: `
-    [detailed Item type definition](api/globals.html#item)
-    `,
-  })(Helmet),
-);
+import './helmet';
+import './i18n';
