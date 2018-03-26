0446a2a1133a59e4aa63fb8efd95e4984cc3eb0c
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-03-26 11:52:02 +0200

    Added story for normal item

--- stories/Item.tsx
+++ stories/Item.tsx
@@ -3,38 +3,62 @@ import { storiesOf } from '@storybook/react';
 
 import { Popup, Rarity } from '../src';
 
-storiesOf('item', module).add('shaped', () => (
-  <Popup
-    item={{
-      base: {
-        name: 'Hubris Circlet',
-      },
-      name: 'Mind Brow',
-      rarity: Rarity.rare,
-      energy_shield: {
-        value: 200,
-        augmented: true,
-      },
-      quality: 6,
-      shaper: true,
-      requirements: {
-        level: {
-          value: 69,
+storiesOf('item', module)
+  .add('shaped', () => (
+    <Popup
+      item={{
+        base: {
+          name: 'Hubris Circlet',
         },
-        intelligence: {
-          value: 154,
+        name: 'Mind Brow',
+        rarity: Rarity.rare,
+        energy_shield: {
+          value: 200,
+          augmented: true,
         },
-      },
-      explicitStats: [
-        'Place an additional Mine',
-        'Socketed Gems are Supported by Level 18 Remote Mine',
-        '16% increased Effect of Chill',
-        'Socketed Gems are Supported by Level 18 Hypothermia',
-        '+1 to Level of Socketed Minion Gems',
-        '+67 to maximum Life',
-        '18% increased Stun and Block Recovery',
-        'Adds 5 to 89 Lightning Damage to Spells',
-      ],
-    }}
-  />
-));
+        quality: 6,
+        shaper: true,
+        requirements: {
+          level: {
+            value: 69,
+          },
+          intelligence: {
+            value: 154,
+          },
+        },
+        explicitStats: [
+          'Place an additional Mine',
+          'Socketed Gems are Supported by Level 18 Remote Mine',
+          '16% increased Effect of Chill',
+          'Socketed Gems are Supported by Level 18 Hypothermia',
+          '+1 to Level of Socketed Minion Gems',
+          '+67 to maximum Life',
+          '18% increased Stun and Block Recovery',
+          'Adds 5 to 89 Lightning Damage to Spells',
+        ],
+      }}
+    />
+  ))
+  .add('normal', () => (
+    <Popup
+      item={{
+        base: {
+          name: 'Hubris Circlet',
+        },
+        rarity: Rarity.normal,
+        energy_shield: {
+          value: 200,
+          augmented: false,
+        },
+        quality: 0,
+        requirements: {
+          level: {
+            value: 69,
+          },
+          intelligence: {
+            value: 154,
+          },
+        },
+      }}
+    />
+  ));
