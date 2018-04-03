b0790267beeb72038ab6bd021329d03b7bf61fc6
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-04-03 13:39:04 +0200

    Fixed fill loading missing transtition on initial render

--- src/components/FillLoading/FillLoading.tsx
+++ src/components/FillLoading/FillLoading.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Transition from 'react-transition-group/Transition';
+import { CSSTransition } from 'react-transition-group';
 import classnames from 'classnames';
 
 import './style.css';
@@ -12,39 +12,10 @@ export interface Props {
 
 const duration = 5000;
 
-// eps1lon: tried to move styles into css but nothing happened
-const default_styles = {
-  height: '100%',
-  width: '0%',
-  transition: `width ${duration}ms`
-};
-
-const transition_styles = {
-  entering: { width: '0%' },
-  entered: { width: '100%' },
-  // accelerate
-  exiting: { width: '100%', transition: `width 1ms` },
-  exited: {
-    width: '100%',
-    opacity: 0,
-    transition: `opacity 500ms ease-out`
-  }
-};
-
 export const FillLoading: React.SFC<Props> = ({ loading }) => {
   return (
-    <Transition in={loading} timeout={duration}>
-      {(state: TransitionState) => {
-        return (
-          <div
-            className="fill-loading"
-            style={{
-              ...default_styles,
-              ...transition_styles[state]
-            }}
-          />
-        );
-      }}
-    </Transition>
+    <CSSTransition in={loading} classNames="" timeout={duration}>
+      <div className="fill-loading" />
+    </CSSTransition>
   );
 };
