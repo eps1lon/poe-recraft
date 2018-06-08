fd5c64a879d724081fd683d4c909c002afc290e4
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-06-08 11:24:47 +0200

    chore(lynt): fix lint and format errors

--- src/__tests__/schema.ts
+++ src/__tests__/schema.ts
@@ -6,32 +6,36 @@ describe.skip('type definitions matching json files in data', () => {
   // be aware that this causes some issues on optional properties and
   // and inferred never[]
   // @see d8535fa8361e706cfafa8b567b49806062e5b1e4
-  type DeepExactify<T, X extends T> = T & {
-    [K in keyof X]: K extends keyof T ? DeepExactify<T[K], X[K]> : never
-  }
+  type DeepExactify<T, X extends T> = T &
+    { [K in keyof X]: K extends keyof T ? DeepExactify<T[K], X[K]> : never };
 
   test('atlas', async () => {
     const json = await import('../../data/atlas.json');
+    // tslint:disable-next-line: no-unused-variable
     const checked: DeepExactify<schema.Atlas, typeof json> = json;
   });
 
   test('craftingbenchoptions', async () => {
     const json = await import('../../data/craftingbenchoptions.json');
+    // tslint:disable-next-line: no-unused-variable
     const checked: schema.CraftingBenchOptions = json;
   });
 
   test('essences', async () => {
     const json = await import('../../data/essences.json');
+    // tslint:disable-next-line: no-unused-variable
     const checked: schema.Essences = json;
   });
 
   test('items', async () => {
     const json = await import('../../data/items.json');
+    // tslint:disable-next-line: no-unused-variable
     const checked: schema.Items = json;
   });
 
   test('mods', async () => {
     const json = await import('../../data/mods.json');
+    // tslint:disable-next-line: no-unused-variable
     const checked: DeepExactify<schema.Mods, typeof json> = json;
   });
-})
+});
