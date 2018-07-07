aeac9606bd15c6b0c0d85cbefb29ce6af6bc36e3
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-07-07 12:59:59 +0200

    chore: removed unused package
    Extending native error class is useless anyway since it will fail
    instanceof checks. If needs arise again we should use e.g. make-error

--- src/exceptions/AbstractMethod.ts
+++
@@ -1,5 +0,0 @@
-export default class AbstractMethod extends Error {
-  constructor(method: string) {
-    super(`override abstract method ${method}`);
-  }
-}
--- src/exceptions/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as AbstractMethod } from './AbstractMethod';
