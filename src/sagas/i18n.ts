6570eaacb4604c1c9124ba0106ae0f226318e7ce
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2018-07-07 09:52:54 +0200

    chore(lint): enable no-var-requires

--- src/sagas/i18n.ts
+++ src/sagas/i18n.ts
@@ -2,7 +2,7 @@ import { addLocaleData } from 'react-intl';
 import { all, call, fork, put, take } from 'redux-saga/effects';
 
 import { i18n_actions } from 'state/i18n';
-const { messages: default_messages } = require('../locales/default');
+import { messages as default_messages } from '../locales/default';
 
 const requireLocaleData = (locale: string) => {
   return import(/* webpackChunkName: "i18n/[request]" */ `../locales/${locale}`);
--- src/state/item/__tests__/snapshotItem.ts
+++ src/state/item/__tests__/snapshotItem.ts
@@ -1,3 +1,4 @@
+// tslint:disable:no-var-requires
 import { createItems, createMods } from 'poe-mods';
 import snapshotItem from '../snapshotItem';
 
