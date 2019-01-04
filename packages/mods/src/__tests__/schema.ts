3b156776875bbbe55ecdddfc7bf1eecb296ff08e
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-01-04 14:11:33 +0100

    test(mods): only test schema on equipment and prefixes
    Full test caused out-of-memory. Equipment and prefixes have
    to suffice

--- packages/mods/src/__tests__/schema.ts
+++ packages/mods/src/__tests__/schema.ts
@@ -3,41 +3,30 @@
 import * as schema from '../schema';
 
 describe.skip('type definitions matching json files in data', () => {
-  // be aware that this causes some issues on optional properties and
-  // and inferred never[]
-  // @see d8535fa8361e706cfafa8b567b49806062e5b1e4
-  type DeepExactify<T, X extends T> = T &
-    { [K in keyof X]: K extends keyof T ? DeepExactify<T[K], X[K]> : never };
-
   test('atlas', async () => {
     const { default: json } = await import('../../data/atlas.json');
-    // tslint:disable-next-line: no-unused-variable
-    const checked: DeepExactify<schema.Atlas, typeof json> = json;
+    const checked: schema.Atlas = json;
   });
 
   test('craftingbenchoptions', async () => {
     const {
       default: json,
     } = await import('../../data/craftingbenchoptions.json');
-    // tslint:disable-next-line: no-unused-variable
     const checked: schema.CraftingBenchOptions = json;
   });
 
   test('essences', async () => {
     const { default: json } = await import('../../data/essences.json');
-    // tslint:disable-next-line: no-unused-variable
     const checked: schema.Essences = json;
   });
 
   test('items', async () => {
-    const { default: json } = await import('../../data/items.json');
-    // tslint:disable-next-line: no-unused-variable
+    const { default: json } = await import('../../data/items/equipment.json');
     const checked: schema.Items = json;
   });
 
   test('mods', async () => {
-    const { default: json } = await import('../../data/mods.json');
-    // tslint:disable-next-line: no-unused-variable
-    const checked: DeepExactify<schema.Mods, typeof json> = json;
+    const { default: json } = await import('../../data/mods/prefixes.json');
+    const checked: schema.Mods = json;
   });
 });
