9c949d98a5cc25f65f26c115ce988d369c230438
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-01-06 15:49:17 +0100

    feat: export poe colors
    Intended for css-in-js solutions

---
+++ packages/react-item/src/colors.ts
@@ -0,0 +1,33 @@
+const craft_color = '#b8daf2';
+const cosmetic_color = '#aa9e82';
+const mod_stats = '#8888ff';
+const property_default = 'white';
+
+export default {
+  // mods
+  mod_stats,
+  explicit_color: mod_stats,
+  implicit_color: mod_stats,
+  utility_color: mod_stats,
+
+  craft_color,
+  crafted_color: craft_color,
+  enchant_color: craft_color,
+
+  cosmetic_color,
+
+  unmet_color: 'red',
+
+  property_default,
+  property_augmented: mod_stats,
+
+  property_physical_damage: property_default,
+  property_chaos_damage: '#d02090',
+  property_cold_damage: '#366492',
+  property_fire_damage: '#960000',
+  property_lightning_damage: 'gold',
+
+  // extended
+  prefix_color: '#7aaff1',
+  suffix_color: '#ec7676',
+};
