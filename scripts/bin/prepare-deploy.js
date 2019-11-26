9bce42f1dc7ba6af33d929bc00c2035aefe354cc
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-11-26 17:32:21 +0100

    chore: Publish docs under /docs (#12)

---
+++ scripts/bin/prepare-deploy.js
@@ -0,0 +1,121 @@
+#!/usr/bin/env node
+/* eslint-env node */
+const fs = require('fs').promises;
+const execa = require('execa');
+const Listr = require('listr');
+const path = require('path');
+
+const workspaceRoot = path.resolve(__dirname, '../../');
+const exportPath = path.resolve(workspaceRoot, './website/build');
+
+main().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
+
+async function main() {
+  const tasks = new Listr([
+    { title: 'bootstrap', task: () => execa('yarn', ['bootstrap']) },
+    {
+      title: 'build:esm',
+      task: () => {
+        return new Listr(
+          [
+            {
+              title: 'i18n',
+              task: () => execa('yarn', ['workspace', 'poe-i18n', 'build:esm']),
+            },
+            {
+              title: 'poe-mods',
+              task: () => execa('yarn', ['workspace', 'poe-mods', 'build:esm']),
+            },
+            {
+              title: 'poe-components-item',
+              task: () =>
+                execa('yarn', [
+                  'workspace',
+                  'poe-components-item',
+                  'build:esm',
+                ]),
+            },
+          ],
+          { concurrent: true },
+        );
+      },
+    },
+    {
+      title: 'build',
+      task: () => {
+        return new Listr(
+          [
+            {
+              title: 'build website',
+              task: () => execa('yarn', ['workspace', 'website', 'build']),
+            },
+            {
+              title: 'build docs/mods',
+              task: () =>
+                execa('yarn', ['workspace', 'poe-mods', 'build:docs']),
+            },
+            {
+              title: 'build docs/react-components/api',
+              task: () =>
+                execa('yarn', ['workspace', 'poe-components-item', 'docs:api']),
+            },
+            {
+              title: 'build docs/react-components/stories',
+              task: () =>
+                execa('yarn', [
+                  'workspace',
+                  'poe-components-item',
+                  'docs:components:build',
+                ]),
+            },
+          ],
+          { concurrent: true },
+        );
+      },
+    },
+    {
+      title: 'prepare export',
+      task: () => {
+        return new Listr([
+          {
+            title: 'prepare docs/',
+            task: () =>
+              fs.mkdir(path.resolve(exportPath, './docs/poe-components-item'), {
+                recursive: true,
+              }),
+          },
+          {
+            title: 'move poe-components-item docs',
+            task: () =>
+              Promise.all([
+                fs.rename(
+                  path.resolve(workspaceRoot, './packages/react-item/docs'),
+                  path.resolve(exportPath, './docs/poe-components-item/api'),
+                ),
+                fs.rename(
+                  path.resolve(workspaceRoot, './packages/react-item/dist'),
+                  path.resolve(
+                    exportPath,
+                    './docs/poe-components-item/stories',
+                  ),
+                ),
+              ]),
+          },
+          {
+            title: 'move poe-mods docs',
+            task: () =>
+              fs.rename(
+                path.resolve(workspaceRoot, './packages/mods/docs-dist'),
+                path.resolve(exportPath, './docs/poe-mods'),
+              ),
+          },
+        ]);
+      },
+    },
+  ]);
+
+  await tasks.run();
+}
